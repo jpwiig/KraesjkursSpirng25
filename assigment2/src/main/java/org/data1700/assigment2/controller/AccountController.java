bb6d476c2a07bf20b52fc576a17703c78235498d
jpwiig <jpwiig@gmail.com>
2025-05-11 21:07:34 +0200

    added the final things before tomorrow

--- assigment2/src/main/java/org/data1700/assigment2/controller/AccountController.java
+++ assigment2/src/main/java/org/data1700/assigment2/controller/AccountController.java
@@ -1,8 +1,15 @@
 package org.data1700.assigment2.controller;
 
+import org.data1700.assigment2.repository.AccountRepository;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class AccountController {
+private final AccountRepository rep;
+
+
+public AccountController(AccountRepository rep){
+    this.rep = rep;
+}
 
 }
--- assigment2/src/main/java/org/data1700/assigment2/repository/AccountRepository.java
+++ assigment2/src/main/java/org/data1700/assigment2/repository/AccountRepository.java
@@ -1,14 +1,22 @@
 package org.data1700.assigment2.repository;
 
 import org.data1700.assigment2.models.Account;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
+import java.sql.JDBCType;
 import java.util.ArrayList;
 import java.util.List;
 
+
+
 @Repository
 public class AccountRepository {
+    private final JdbcTemplate db;
 
+    public AccountRepository(JdbcTemplate db){
+        this.db=db;
+    }
     public void load(){
         //as no db is added:
 
